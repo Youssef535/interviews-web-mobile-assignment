155eb5cd2772cec1e4057967e399772f6035ecae
youssef535 <youssefsoltani53@yahoo.com>
2023-01-22 17:09:46 +0100

    Clean up the project

--- Xtr-Project/src/App.jsx
+++ Xtr-Project/src/App.jsx
@@ -1,12 +1,18 @@
 import { React } from 'react'
 import './App.css'
+import axios from 'axios';
 
 
 function App() {
+  
   return (
     <div className="App">
       
       <h1>hello vite</h1>
+      <h3>sdsdsd</h3>
+
+      
+    
     </div>
   )
 }
