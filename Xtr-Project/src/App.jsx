08fda6a777f69aa821d2f8edba09478c7b5c126a
youssef535 <youssefsoltani53@yahoo.com>
2023-01-22 20:35:30 +0100

    Updating the project

--- Xtr-Project/src/App.jsx
+++ Xtr-Project/src/App.jsx
@@ -1,15 +1,14 @@
 import { React } from 'react'
 import './App.css'
-import axios from 'axios';
+
 
 
 function App() {
-  
   return (
     <div className="App">
       
-      <h1>hello vite</h1>
-      
+      <h1>Hello Vite Project</h1>
+
     </div>
   )
 }
