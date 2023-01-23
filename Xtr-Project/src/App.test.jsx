4ca0d5a700dce9a85fb2015dd536f440fde29333
youssef535 <youssefsoltani53@yahoo.com>
2023-01-23 14:27:32 +0100

    Fixing bugs and Updating the app

--- Xtr-Project/src/App.test.jsx
+++ Xtr-Project/src/App.test.jsx
@@ -1,9 +1,18 @@
-import { it, describe } from "vitest";
-import {render, screen } from "@testing-library/react";
+import { it, describe, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 // We will follow 3 steps to test our component in the future
 
-describe ('App', () => {
-  it('renders Hello Vite Project', () => {
-    
-  })
+describe("App", () => {
+  it("renders Hello Vite Project", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    // Expect
+    expect(
+      screen.getByRole("heading", {
+        level: 1,
+      })
+    ).toHaveTextContent("Hello Vite Project");
+  });
 });
