2411a166ecb645ec01de51339943fcdc6753436c
youssef535 <youssefsoltani53@yahoo.com>
2023-01-23 13:23:48 +0100

    Working on Testing Unit and updating the app

---
+++ Xtr-Project/src/App.test.jsx
@@ -0,0 +1,9 @@
+import { it, describe } from "vitest";
+import {render, screen } from "@testing-library/react";
+// We will follow 3 steps to test our component in the future
+
+describe ('App', () => {
+  it('renders Hello Vite Project', () => {
+    
+  })
+});
